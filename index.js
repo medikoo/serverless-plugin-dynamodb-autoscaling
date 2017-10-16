6c998c65a6fc3c6ba10f5dfe3c54be83369d44d1
Mariusz Nowak <medyk@medikoo.com>
2017-10-16 12:41:59 +0200

    refactor: integrate resources generation into class

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 "use strict";
 
-const compact           = require("es5-ext/array/#/compact")
-    , assignDeep        = require("es5-ext/object/assign-deep")
-    , copyDeep          = require("es5-ext/object/copy-deep")
-    , isValue           = require("es5-ext/object/is-value")
-    , isObject          = require("es5-ext/object/is-object")
-    , objToArray        = require("es5-ext/object/to-array")
-    , objForEach        = require("es5-ext/object/for-each")
-    , objMap            = require("es5-ext/object/map")
-    , minimatch         = require("minimatch")
-    , generateResources = require("./lib/generate-resources");
+const compact       = require("es5-ext/array/#/compact")
+    , assignDeep    = require("es5-ext/object/assign-deep")
+    , copyDeep      = require("es5-ext/object/copy-deep")
+    , isEmpty       = require("es5-ext/object/is-empty")
+    , isValue       = require("es5-ext/object/is-value")
+    , isObject      = require("es5-ext/object/is-object")
+    , objToArray    = require("es5-ext/object/to-array")
+    , objForEach    = require("es5-ext/object/for-each")
+    , objMap        = require("es5-ext/object/map")
+    , capitalize    = require("es5-ext/string/#/capitalize")
+    , hyphenToCamel = require("es5-ext/string/#/hyphen-to-camel")
+    , minimatch     = require("minimatch");
 
 const dimensionDefaults = { minCapacity: 5, maxCapacity: 200, targetUsage: 0.75 }
     , entityDefaults = { read: dimensionDefaults, write: dimensionDefaults }
@@ -19,6 +21,50 @@ const dimensionDefaults = { minCapacity: 5, maxCapacity: 200, targetUsage: 0.75 
 const entitySettingNames = new Set(
 	Object.keys(entityDefaults).concat(Object.keys(entityDefaults.read))
 );
+
+const resolveIndexToken = indexName =>
+	indexName ? capitalize.call(hyphenToCamel.call(indexName).replace(/[^a-zA-Z0-9]/g, "")) : "";
+
+const scalingRoleResource = {
+	Type: "AWS::IAM::Role",
+	Properties: {
+		AssumeRolePolicyDocument: {
+			Version: "2012-10-17",
+			Statement: [
+				{
+					Effect: "Allow",
+					Principal: { Service: ["application-autoscaling.amazonaws.com"] },
+					Action: ["sts:AssumeRole"]
+				}
+			]
+		},
+		Path: "/",
+		Policies: [
+			{
+				PolicyName: "root",
+				PolicyDocument: {
+					Version: "2012-10-17",
+					Statement: [
+						{
+							Effect: "Allow",
+							Action: [
+								"dynamodb:DescribeTable",
+								"dynamodb:UpdateTable",
+								"cloudwatch:PutMetricAlarm",
+								"cloudwatch:DescribeAlarms",
+								"cloudwatch:GetMetricStatistics",
+								"cloudwatch:SetAlarmState",
+								"cloudwatch:DeleteAlarms",
+								"autoscaling:*"
+							],
+							Resource: "*"
+						}
+					]
+				}
+			}
+		]
+	}
+};
 
 module.exports = class ServerlessPluginDynamodbAutoscaling {
 	constructor(serverless) {
@@ -41,8 +87,10 @@ module.exports = class ServerlessPluginDynamodbAutoscaling {
 		return pluginConfig;
 	}
 	configure() {
-		Object.assign(this.resources, generateResources(this.config));
+		Object.assign(this.resources, this.generateResources());
 	}
+
+	// Configuration resolution
 	get config() {
 		const resolvedPluginConfig = objMap(this.pluginConfig, config =>
 			this.resolveTableConfig(config));
@@ -115,5 +163,105 @@ module.exports = class ServerlessPluginDynamodbAutoscaling {
 			maxCapacity: config.maxCapacity || dimensionDefaults.maxCapacity,
 			targetUsage: config.targetUsage || dimensionDefaults.targetUsage
 		};
+	}
+
+	// Autoscaling resources generation
+	generateResources() {
+		const addonResources = {};
+		delete this.lastPolicyResourceName;
+		for (const tableConfig of this.config) {
+			if (tableConfig.config) {
+				Object.assign(
+					addonResources,
+					this.generateTableResources(tableConfig.resource, tableConfig.config)
+				);
+			}
+		}
+		if (!isEmpty(addonResources)) addonResources.DynamodbAutoscalingRole = scalingRoleResource;
+		return addonResources;
+	}
+
+	generateTableResources(resource, config) {
+		const resources = {};
+		if (config.table) {
+			Object.assign(resources, this.generateEntityResources(config.table, resource.name));
+		}
+		if (!config.indexes) return resources;
+		const globalSecondaryIndexes = resource.value.Properties.GlobalSecondaryIndexes;
+		if (!Array.isArray(globalSecondaryIndexes)) return resources;
+		for (const index of globalSecondaryIndexes) {
+			const indexName = index.IndexName;
+			Object.assign(
+				resources,
+				this.generateEntityResources(
+					config.indexes[indexName] || config.indexes["*"],
+					resource.name,
+					indexName
+				)
+			);
+		}
+		return resources;
+	}
+	generateEntityResources(config, tableResourceName, indexName = null) {
+		const resources = {};
+		if (config.read) {
+			Object.assign(
+				resources,
+				this.generateDimensionResources("read", config.read, tableResourceName, indexName)
+			);
+		}
+		if (config.write) {
+			Object.assign(
+				resources,
+				this.generateDimensionResources("write", config.write, tableResourceName, indexName)
+			);
+		}
+		return resources;
+	}
+	generateDimensionResources(mode, config, tableResourceName, indexName = null) {
+		const modeCapitalized = capitalize.call(mode);
+		const resourceNamePrefix = `${ tableResourceName }${ resolveIndexToken(
+			indexName
+		) }${ modeCapitalized }`;
+
+		const targetResourceName = `${ resourceNamePrefix }ScalableTarget`;
+		const policyResourceName = `${ resourceNamePrefix }ScalingPolicy`;
+		const resourceAddress = ["table", { Ref: tableResourceName }];
+		if (indexName) resourceAddress.push("index", indexName);
+
+		const resources = {
+			[targetResourceName]: {
+				Type: "AWS::ApplicationAutoScaling::ScalableTarget",
+				Properties: {
+					MaxCapacity: config.maxCapacity,
+					MinCapacity: config.minCapacity,
+					ResourceId: { "Fn::Join": ["/", resourceAddress] },
+					RoleARN: { "Fn::GetAtt": "DynamodbAutoscalingRole.Arn" },
+					ScalableDimension: `dynamodb:${ indexName
+						? "index"
+						: "table" }:${ modeCapitalized }CapacityUnits`,
+					ServiceNamespace: "dynamodb"
+				}
+			},
+			[policyResourceName]: {
+				Type: "AWS::ApplicationAutoScaling::ScalingPolicy",
+				DependsOn: this.lastPolicyResourceName,
+				Properties: {
+					PolicyName: `${ modeCapitalized }AutoScalingPolicy`,
+					PolicyType: "TargetTrackingScaling",
+					ScalingTargetId: { Ref: targetResourceName },
+					TargetTrackingScalingPolicyConfiguration: {
+						TargetValue: config.targetUsage * 100,
+						ScaleInCooldown: 60,
+						ScaleOutCooldown: 60,
+						PredefinedMetricSpecification: {
+							PredefinedMetricType: `DynamoDB${ modeCapitalized }CapacityUtilization`
+						}
+					}
+				}
+			}
+		};
+		this.lastPolicyResourceName = policyResourceName;
+		return resources;
 	}
 };
