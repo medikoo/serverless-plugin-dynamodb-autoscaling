31a2f7d18f4c387044171ba2756276d51d22063d
Mariusz Nowak <medyk@medikoo.com>
2017-10-16 11:31:50 +0200

    refactor: introduce `configure` method

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ const resolveConfig     = require("./lib/resolve-config")
 
 module.exports = class ServerlessPluginDynamodbAutoscaling {
 	constructor(serverless) {
+		this.serverless = serverless;
 		this.hooks = {
-			"after:aws:package:finalize:mergeCustomProviderResources": () => {
-				const resources =
-					serverless.service.provider.compiledCloudFormationTemplate.Resources;
-				Object.assign(
-					resources,
-					generateResources(
-						resolveConfig(resources, serverless.service.custom.dynamodbAutoscaling)
-					)
-				);
-			}
+			"after:aws:package:finalize:mergeCustomProviderResources": this.configure.bind(this)
 		};
+	}
+	configure() {
+		const resources = this.serverless.service.provider.compiledCloudFormationTemplate.Resources;
+		Object.assign(
+			resources,
+			generateResources(
+				resolveConfig(resources, this.serverless.service.custom.dynamodbAutoscaling)
+			)
+		);
 	}
 };
